31ce55dc5ec89fc44d197f3d58414e6dfbe61da1
Alex David <alexdavid@users.noreply.github.com>
2020-06-18 20:49:58 -0700

    Add initial spring boot demo application (#7)

---
+++ src/com/acme/web/LoginController.java
@@ -0,0 +1,32 @@
+package com.acme.web;
+
+import com.acme.shared.Hello;
+import com.acme.shared.Login;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+  @GetMapping("/login")
+  public String getLogin() throws IOException {
+    byte[] file = Files.readAllBytes(Paths.get("src/frontend/index.html"));
+    return new String(file, Charset.forName("UTF-8"));
+  }
+
+  @PostMapping(path="/login", consumes = "application/json")
+  public String postLogin(@RequestBody Map<String, String> body) {
+    if (Login.checkCredentials(body.get("username"), body.get("password"))) {
+      return Hello.greet(body.get("username"));
+    } else {
+      return "Invalid username/password";
+    }
+  }
+}
